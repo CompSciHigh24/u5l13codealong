e0c1dcf6371fedb430f6ab5a68c03a3b2ca21912
CompSciHigh24 <csh-github@compscihigh.org>
2026-04-20 14:22:15 -0400

    Create ratingScript.js

---
+++ public/ratingScript.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const bookData = new FormData(form)
+    const req = Object.fromEntries(bookData)
+
+    const response = await fetch("/add/rating", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        }, 
+        body: JSON.stringify(req)
+    })
+    const data = await response.json()
+    console.log(data)
+
+    window.location.href = "/ratings"
+})
+
+// write the async function deleteRating
+// make sure it redirects to /ratings after
+
